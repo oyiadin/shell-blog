a7cc1560b02c504c7859ca48251fc92fc3a2121f
NAME HERE <email@example.com>
2018-12-22 00:26:51 +0800

    add header.sh

--- index.sh
+++ index.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo "Content-type: text/plain"
-echo ""
+./header.sh
 echo "root@you ~:# ssh ricterz.me"
 echo ""
 
---
+++ timeline.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./header.sh
+if [[ $HTTP_USER_AGENT == *"curl"* ]]; then
+    bgmi history
+else
+    bgmi history | sed $'s/\x1b\[1;[0-9]*m//g' | sed $'s/\x1b\[0m//g'
+fi
